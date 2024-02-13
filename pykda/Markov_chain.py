4025e03aebfb703bf8866b4e0ef147fe35e650f3
Joost Berkhout <joost.berkhout@vu.nl>
2024-02-13 08:45:50 +0100

    Removing files from Git tracking and update gitignore

--- pykda/Markov_chain.py
+++ pykda/Markov_chain.py
@@ -20,7 +20,7 @@ class MarkovChain:
         self.bUniChain = True
 
     def pi(self):
-        """Stationary distribution (if it exists). """
+        """Stationary distribution (if it exists)."""
 
         if self.bUniChain:
             # stationary distribution exists
@@ -29,16 +29,18 @@ class MarkovChain:
             # TODO: instead of inverse, solve a system of linear equations
             return la.inv(Z)[[0], :]
         else:
-            raise Warning('Stationary distribution does not exist.')
+            raise Warning("Stationary distribution does not exist.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     P = np.array(
-        [[0.    ,     0.52493781, 0.47506219],
-         [0.       ,  0.66666667 ,0.33333333],
-         [0.06666667 ,0.33333333 ,0.6       ]]
-        )
+        [
+            [0.0, 0.52493781, 0.47506219],
+            [0.0, 0.66666667, 0.33333333],
+            [0.06666667, 0.33333333, 0.6],
+        ]
+    )
     MC = MarkovChain(P)
 
     print(MC.pi())
